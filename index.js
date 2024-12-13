e40e033ebbf76f254df0b00a5b631514ec4bd088
NjotchoBrandyCyrsu <njotchiobrandycyrus@gmail.com>
2024-12-14 00:28:24 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
+const dbConnect = require('./libs/dbConnect');
+require('dotenv').config();
+require('./libs/dbConnect');
 
 const app = express();
-
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-
 app.use(morgan('dev'));
-
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Hello From Node.js' });
+  res.render('index', { message: 'Hello From Node.js' });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('index', { message: 'The Contact Page' });
+  res.render('index', { message: 'The Contact Page' });
 });
 
 app.get('/about', (req, res) => {
-    res.render('index', { message: 'The About Page' });
+  res.render('index', { message: 'The About Page' });
 });
 
 app.get('*', (req, res) => {
-    res.status(404).render('index', { message: 'Not Found' });
+  res.status(404).render('index', { message: 'Not Found' });
 });
-
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-
